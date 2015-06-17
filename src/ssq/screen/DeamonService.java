20eee1de011904b2cc12e0f77cb56031dc55e04b
ssqstone <834165855@qq.com>
2015-06-17 16:43:28 +0800

    修复一个一直初始化工具条的bug

--- src/ssq/screen/DeamonService.java
+++ src/ssq/screen/DeamonService.java
@@ -18,11 +18,11 @@ public class DeamonService extends Service
     {
         return null;
     }
-
+    
     protected class StoppableRunnable implements Runnable
     {
         protected ConditionVariable mConditionVariable = new ConditionVariable(false);
-
+        
         @Override
         public final void run()
         {
@@ -35,75 +35,75 @@ public class DeamonService extends Service
                 }
             }
         }
-
+        
         protected void work()
         {
         }
-
+        
         private void start()
         {
             new Thread(this).start();
         }
-
+        
         public void stop()
         {
             mConditionVariable.open();
         }
     }
-
+    
     private boolean           filterHidenBecauseOfInstall   = false;
     private boolean           tooletHidenBecauseOfPhoneCall = false;
-
+    
     private StoppableRunnable deamonThread                  = new StoppableRunnable()
-    {
-        @Override
-        protected void work()
-        {
-            ActivityManager activityManager = (ActivityManager) Scruin.getApp().getSystemService(ACTIVITY_SERVICE);
-            List<RunningTaskInfo> runningTaskInfos = activityManager.getRunningTasks(1);
-            RunningTaskInfo runningTaskInfo = runningTaskInfos.get(0);
-
-            ComponentName localComponentName = runningTaskInfo.topActivity;
-            String packageName = localComponentName.getPackageName();
-
-            if (packageName.equals("com.android.packageinstaller"))
-            {
-                if (!filterHidenBecauseOfInstall)
-                {
-                    Message msg = new Message();
-                    msg.what = Scruin.TOAST;
-                    msg.obj = "应用安装过程中会暂停显示护目镜";
-                    Scruin.handler.sendMessage(msg);
-                    Scruin.handler.sendEmptyMessage(Scruin.REMOVE_FILTER);
-                    filterHidenBecauseOfInstall = true;
-                }
-            }
-            else if (filterHidenBecauseOfInstall)
-            {
-                Scruin.handler.sendEmptyMessage(Scruin.SHOW_FILTER);
-                filterHidenBecauseOfInstall = false;
-            }
-
-            if (packageName.equals("com.android.incallui"))
-            {
-                if (!tooletHidenBecauseOfPhoneCall)
-                {
-                    Message msg = new Message();
-                    msg.what = Scruin.TOAST;
-                    msg.obj = "通话过程中会暂停通知栏拖动功能, 防止耳朵碰到";
-                    Scruin.handler.sendMessage(msg);
-                    Scruin.handler.sendEmptyMessage(Scruin.REMOVE_TOOLET);
-                    tooletHidenBecauseOfPhoneCall = true;
-                }
-            }
-            else
-            {
-                Scruin.handler.sendEmptyMessage(Scruin.SHOW_TOOLET);
-                tooletHidenBecauseOfPhoneCall = false;
-            }
-        }
-    };
-
+                                                            {
+                                                                @Override
+                                                                protected void work()
+                                                                {
+                                                                    ActivityManager activityManager = (ActivityManager) Scruin.getApp().getSystemService(ACTIVITY_SERVICE);
+                                                                    List<RunningTaskInfo> runningTaskInfos = activityManager.getRunningTasks(1);
+                                                                    RunningTaskInfo runningTaskInfo = runningTaskInfos.get(0);
+                                                                    
+                                                                    ComponentName localComponentName = runningTaskInfo.topActivity;
+                                                                    String packageName = localComponentName.getPackageName();
+                                                                    
+                                                                    if (packageName.equals("com.android.packageinstaller"))
+                                                                    {
+                                                                        if (!filterHidenBecauseOfInstall)
+                                                                        {
+                                                                            Message msg = new Message();
+                                                                            msg.what = Scruin.TOAST;
+                                                                            msg.obj = "应用安装过程中会暂停显示护目镜";
+                                                                            Scruin.handler.sendMessage(msg);
+                                                                            Scruin.handler.sendEmptyMessage(Scruin.REMOVE_FILTER);
+                                                                            filterHidenBecauseOfInstall = true;
+                                                                        }
+                                                                    }
+                                                                    else if (filterHidenBecauseOfInstall)
+                                                                    {
+                                                                        Scruin.handler.sendEmptyMessage(Scruin.SHOW_FILTER);
+                                                                        filterHidenBecauseOfInstall = false;
+                                                                    }
+                                                                    
+                                                                    if (packageName.equals("com.android.incallui"))
+                                                                    {
+                                                                        if (!tooletHidenBecauseOfPhoneCall)
+                                                                        {
+                                                                            Message msg = new Message();
+                                                                            msg.what = Scruin.TOAST;
+                                                                            msg.obj = "通话过程中会暂停通知栏拖动功能, 防止耳朵碰到";
+                                                                            Scruin.handler.sendMessage(msg);
+                                                                            Scruin.handler.sendEmptyMessage(Scruin.REMOVE_TOOLET);
+                                                                            tooletHidenBecauseOfPhoneCall = true;
+                                                                        }
+                                                                    }
+                                                                    else if (tooletHidenBecauseOfPhoneCall)
+                                                                    {
+                                                                        Scruin.handler.sendEmptyMessage(Scruin.SHOW_TOOLET);
+                                                                        tooletHidenBecauseOfPhoneCall = false;
+                                                                    }
+                                                                }
+                                                            };
+    
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
@@ -111,7 +111,7 @@ public class DeamonService extends Service
         deamonThread.start();
         return START_STICKY;
     }
-
+    
     @Override
     public void onDestroy()
     {
